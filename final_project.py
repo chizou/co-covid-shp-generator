07c3f70cb17307bd9a553e74667f5a67c6769b65
Chou <Adam.Chou@radiantsolutions.com>
2020-05-25 14:53:51 -0600

    rename downloader and convert final_project to main

--- final_project.py
+++ final_project.py
@@ -5,16 +5,32 @@
 # The application then processes the county specific data into the attribute table of a
 # Colorado by county shapefile
 
-from cocovidprocessor import gdrivedownloader as gd
+from cocovidprocessor import downloader
+from cocovidprocessor import processor
 import yaml
+import sys
 
-with open("config.yml") as configfh:
-    try:
-        config = yaml.safe_load(configfh)
-        gdrive_creds = config['google-drive-creds']
-    except yaml.YAMLError as e:
-        print(f"Exiting... Error loading config: {e}")
-        sys.exit()
+def load_configs():
+    with open("config.yml") as configfh:
+        try:
+            return yaml.safe_load(configfh)
+        except yaml.YAMLError as e:
+            print(f"Exiting... Error loading config: {e}")
+            sys.exit()
 
-downloader = gd.GDriveDownloader(api_key=gdrive_creds)
-downloader.download()
+
+def main():
+    configs = load_configs()
+    gdrive_creds = configs['google-drive-creds']
+
+    # First, download the csv files using our downloader class
+    download = downloader.Downloader(api_key=gdrive_creds)
+    download.download(verbose=True)
+
+    # Once we have the files, process them
+    process = processor.CsvProcessor()
+
+if __name__ == "__main__":
+    main()
+
+
